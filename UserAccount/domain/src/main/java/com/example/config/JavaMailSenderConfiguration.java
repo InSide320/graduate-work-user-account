ba4afbf16214c0c5d869f72caf6d127e228590cd
Denis Kud <dekud2109@gmail.com>
2022-06-22 15:00:15 +0300

    Changed approach to disable debug mail

--- UserAccount/domain/src/main/java/com/example/config/JavaMailSenderConfiguration.java
+++ UserAccount/domain/src/main/java/com/example/config/JavaMailSenderConfiguration.java
@@ -38,7 +38,6 @@ public class JavaMailSenderConfiguration {
         Properties properties = mailSender.getJavaMailProperties();
         properties.setProperty("mail.smtp.auth", smtp);
         properties.setProperty("mail.smtp.starttls.enable", starttls);
-        debug = "false";
         properties.setProperty("mail.debug", debug);
         properties.setProperty("mail.smtp.ssl.trust", host);
 
